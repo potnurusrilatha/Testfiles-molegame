c160e56fba00ae1ea102f92a9fe88adee6818e76
Srilatha <119068383+potnurusrilatha@users.noreply.github.com>
2025-08-20 11:42:54 +0200

    added Header

---
+++ src/component/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const Header = () => {
+    return (
+        
+        <header className="p-4">
+            <h1 className="text-center font-bold">Whack a Mole</h1>
+        </header>
+        
+    )
+}
+export default Header
