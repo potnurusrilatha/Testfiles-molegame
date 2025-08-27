c314de4877282ceb980d8c47ced8fd640d80ea29
Srilatha <119068383+potnurusrilatha@users.noreply.github.com>
2025-08-27 14:16:53 +0200

    change

--- src/component/Footer/index.tsx
+++ src/component/Footer/index.tsx
@@ -2,10 +2,8 @@ import { FooterProps } from "@/utils/types";
 
 const Footer = ({developer = "Srilatha", version = "1.0.0" }:FooterProps) => {
     return (
-        <footer data-testid="footer" className="text-center" role="contentinfo">
-            <p>Developed by {developer} </p>
-            <p>Version: {version} </p>
-            <p>&copy; {new Date().getFullYear()}</p>
+        <footer className="w-full py-6 mt-auto text-center bg-red-400 text-stone-200" data-testid="footer"  role="contentinfo">
+             <p>&copy; {new Date().getFullYear()} {developer} | Version {version}</p>
         </footer>
     )
 }
