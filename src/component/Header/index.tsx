124f3e07e1b2ec2a1044db6d8339fe7aa4db811a
Srilatha <119068383+potnurusrilatha@users.noreply.github.com>
2025-08-27 22:05:16 +0200

    removed bold

--- src/component/Header/index.tsx
+++ src/component/Header/index.tsx
@@ -3,7 +3,7 @@ const Header = () => {
     return (
         
         <header className="bg-gradient-to-r from-purple-500 to-pink-500 p-6 shadow-md">
-            <h1 className="text-center text-white font-extrabold text-3xl tracking-wide drop-shadoe-lg">Whack a Mole</h1>
+            <h1 className="text-center text-white font-bold text-3xl tracking-wide drop-shadoe-lg">Whack a Mole</h1>
         </header>
         
     )
