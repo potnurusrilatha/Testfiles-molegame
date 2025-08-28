0f477ac4d735f0e27a508dc86b6a03b6c55ecc67
Srilatha <119068383+potnurusrilatha@users.noreply.github.com>
2025-08-28 11:55:44 +0200

    added data-testid

--- src/component/ControlPanel/index.tsx
+++ src/component/ControlPanel/index.tsx
@@ -3,7 +3,8 @@ import { ControlPanelProps } from '@/utils/types';
 
 const ControlPanel = ({start, reset}:ControlPanelProps) => {
   return (
-    <div className="flex justify-center gap-4 mt-4">
+    <div data-testid="controlpanel"
+     className="flex justify-center gap-4 mt-4">
       <button data-testid="start-btn"
                 onClick={start}
                 className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300">
